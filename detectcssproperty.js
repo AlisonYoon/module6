e2afeb82d21062452488249344fae1a68c775e6e
AlisonYoon <politescarlett1@gmail.com>
2019-08-28 14:46:40 +0100

    detect if a specific css property is available in user s browser

---
+++ detectcssproperty.js
@@ -0,0 +1,6 @@
+if(document.querySelector('div').style.borderRadius) {
+    //apply the class that uses border-radius
+} else {
+    //apply a different class that doesn't
+    //use border-radius
+}
