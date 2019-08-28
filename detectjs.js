70b29366ab7f602a735ee9bc97472a1fb08e432c
AlisonYoon <politescarlett1@gmail.com>
2019-08-28 14:39:13 +0100

    use JS and CSS to detect if JS is on in user s browser

---
+++ detectjs.js
@@ -0,0 +1,4 @@
+document.querySelector('.no-js').classList.remove('no-js')
+
+//no-js class has {display: none}, so if JS runs correctly, it will remove that class and everything will show just fine.
+//If user's browser turned off JS, then nothing will show.
