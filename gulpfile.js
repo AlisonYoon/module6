c950da79e46f152f85e74c1e3fe912a82331e0e0
AlisonYoon <politescarlett1@gmail.com>
2019-08-29 13:54:55 +0100

    gult.task()

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp')
+var sass = require('gulp-sass')  // gulp-sass is a function
 
 gulp.task('hello', function(done) {  //passing in a built-in function-how it's called doesn't matter- as a parameter
     console.log('howdy')
+    done()
+})
+
+gulp.task('compile', function(done) {
+    return gulp.src('scss.scss')   //gulp.src says "find this file"
+        .pipe(sass())   // pipe() makes it each line goes synchronously
+        .pipe(gulp.dest('.'))   // gulp.dest() -> where the output goes to. "." means where I am now.
+    done()
+})
+
+gulp.task('sass:watch', function(done) {
+    gulp.watch('scss.scss', gulp.series('compile'))    //gulp.series() is the list of tasks gulp can run
     done()
 })
