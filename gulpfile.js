c182dafe315af9bde6bb729f066662983e1460a2
AlisonYoon <politescarlett1@gmail.com>
2019-08-29 14:42:37 +0100

    add sourcemaps in the gulp sass compiled css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')  // gulp-sass is a function
+var sourcemap = require('gulp-sourcemaps')
+
+sass.compiler = function('node-sass')
 
 gulp.task('hello', function(done) {  //passing in a built-in function-how it's called doesn't matter- as a parameter
     console.log('howdy')
@@ -10,6 +13,15 @@ gulp.task('compile', function(done) {
     return gulp.src('scss.scss')   //gulp.src says "find this file"
         .pipe(sass())   // pipe() makes it each line goes synchronously
         .pipe(gulp.dest('.'))   // gulp.dest() -> where the output goes to. "." means where I am now.
+    done()
+})
+
+gulp.task('compileAndDoSourceMaps', function (done) {
+    return gulp.src('scss.scss')
+        .pipe(sourcemap.init())
+        .pipe(sass())
+        .pipe(sourcemap.write)
+        .pipe(gulp.dest('.'))
     done()
 })
 
